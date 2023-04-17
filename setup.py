d445479ee0b359e4319c4dfe6adbe957eafb2883
Jiayao Yan <45738410+jiy352@users.noreply.github.com>
2023-04-17 13:39:15 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VLM_package',
-    version='-1',
+    version='0.1',
     packages=find_packages(),
     install_requires=[
         # 'numpy',
